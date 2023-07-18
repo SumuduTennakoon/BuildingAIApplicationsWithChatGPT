8feb389193aa3aba17de47e2711ae875a8724810
SumuduTennakoon <59441388+SumuduTennakoon@users.noreply.github.com>
2023-07-18 00:20:40 -0400

    init

---
+++ DemoApp/ChatGPT_Demo_App.py
@@ -0,0 +1,12 @@
+# Demo App: Building AI Applications with ChatGPT and OpenAI LLMs
+# by Sumudu Tennakoon
+# Last Update: 2022-07-09
+# streamlit run .GitHub\BuildingAIApplicationsWithChatGPT-main\ChatGPT_Demo_App.py
+
+import streamlit as st
+
+st.header("Building AI Applications with ChatGPT - OpenAI Demo App")
+st.subheader("Sumudu Tennakoon")
+st.write("https://github.com/SumuduTennakoon/BuildingAIApplicationsWithChatGPT")
+
+
